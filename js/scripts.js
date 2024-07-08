c4b2c3aee06f870067cd6326ac835b5e4e87c7bb
Enzo Muzi <enzshadows@gmail.com>
2024-07-08 17:50:46 -0300

    Funcionalidades adicionadas nos botões

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,7 @@ const saveTodo = (text) => {
     todoList.appendChild(todo);
 
     todoInput.value = "";
+    todoInput.focus();
 
 }
 // Events
@@ -48,3 +49,22 @@ todoForm.addEventListener("submit", (e) => {
        saveTodo(inputValue)                 // save todo
     }
 });
+
+// Este trecho adiciona um event listener ao documento (a página HTML) que espera por cliques em qualquer lugar do documento. Quando ocorre um clique, a função definida como segundo argumento será executada. A função recebe o evento de clique (e) como parâmetro.
+document.addEventListener("click", (e) => { 
+
+    const targetEl = e.target; // e.target representa o elemento específico que foi clicado durante o evento. Este elemento é armazenado na constante targetEl.
+    const parentEl = targetEl.closest("div"); // targetEl.closest("div") procura pelo ancestral mais próximo (ou seja, pai, avô, bisavô, etc.) do elemento targetEl que corresponda ao seletor especificado, neste caso, um elemento <div>. O elemento encontrado é armazenado na constante parentEl.
+    
+    if(targetEl.classList.contains("finish-todo")) { // Este bloco condicional verifica se o elemento clicado (targetEl) possui a classe CSS "finish-todo". Se possuir, o console irá imprimir "Clicou para finalizar".
+        parentEl.classList.toggle("done");
+    }
+
+    if(targetEl.classList.contains("remove-todo")) {
+        parentEl.remove();
+    }
+
+    if(targetEl.classList.contains("edit-todo")) {
+        
+    }
+})
